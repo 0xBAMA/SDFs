473364a3c114c1381cd1d5cb8bfe84c49f181e3c
0xBAMA <jon.baker1222@gmail.com>
2021-04-10 21:29:09 -0400

    dither info

--- resources/engine_code/shaders/dither.cs.glsl
+++ resources/engine_code/shaders/dither.cs.glsl
@@ -8,8 +8,13 @@ layout( binding = 1 ) uniform sampler2D bayer_dither_pattern;
 layout( binding = 2 ) uniform sampler2D blue_noise_dither_pattern;
 
 // bayer is static, but blue cycles over time, like https://www.shadertoy.com/view/wlGfWG
-uniform int ditherswitch;
-uniform float time;
+uniform int spaceswitch; // what color space
+uniform int dithermode; // methodology (bitcrush blue, bitcrush bayer, exponential blue, exponential bayer)
+uniform float time;    // used to cycle the blue noise values over time
+
+// key thing is to have RGB->colorspace and colorspace->RGB for each colorspace to be used
+// need to refer to the old code, as well as a few shadertoy examples for different spaces
+// trying a new dithering method now based on the above linked shadertoy example
 
 void main()
 {
